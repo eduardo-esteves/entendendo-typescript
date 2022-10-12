62dc0646b9d7e018aec322547a7b35ef83aac4a3
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2022-10-11 21:28:24 -0300

    declarando multiplos parametros com suas tipagens e tipagem no retorno

--- funcoes/parametros.ts
+++ funcoes/parametros.ts
@@ -1,3 +1,6 @@
 const retornaNome = (nome: string): string => nome
 
 console.log(retornaNome('Eduardo'))
+
+const multiplicar = (num1: number, num2: number): number => num1 * num2
+console.log(multiplicar('5', 8))
