91c152f50b13f76840418df3db8fde669d26265c
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2022-10-13 20:58:59 -0300

    Definindo os types para o desafio da aula 32

--- desafios/tipando-obj.ts
+++ desafios/tipando-obj.ts
@@ -1,11 +1,22 @@
-const contaBancaria = {
+interface Conta {
+  saldo: number
+  depositar: (val: number) => void
+}
+
+interface contaCorrente {
+  nome: string
+  contaBancaria: Conta
+  contatos: string[]
+}
+
+const contaBancaria: Conta = {
   saldo: 3456,
   depositar (valor) {
     this.saldo += valor
   }
 }
 
-const correntista = {
+const correntista: contaCorrente = {
   nome: 'Ana Silva',
   contaBancaria,
   contatos: ['54545454', '988888777']
