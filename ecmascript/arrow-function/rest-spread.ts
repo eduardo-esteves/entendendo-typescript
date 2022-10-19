45f45249581a1ba410332dbce11e6a34ffa231e0
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2022-10-19 20:09:18 -0300

    exemplo de uso do spread operator

---
+++ ecmascript/arrow-function/rest-spread.ts
@@ -0,0 +1,9 @@
+// Spread Operator
+const numbers = [1, 90, 100, 400]
+console.log(Math.max(...numbers))
+
+// Spread Operator
+const turmaA: string[] = ['João', 'Maria', 'Fernanda']
+const turmaB: string[] = ['Fernando', 'Miguel', 'Lorena', ...turmaA]
+
+console.log(turmaB)
