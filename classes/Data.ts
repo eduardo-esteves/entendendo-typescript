7ed05afb9654d7784efe05c62094424f783c4613
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2022-10-24 20:08:26 -0300

    example for class in JS with sintax sugar to facilitate on code sintax

---
+++ classes/Data.ts
@@ -0,0 +1,16 @@
+class Data {
+  dia: number
+  mes: number
+  ano: number
+
+  constructor (dia: number, mes: number, ano: number) {
+    this.dia = dia
+    this.mes = mes
+    this.ano = ano
+  }
+}
+
+const aniversario = new Data(3, 11, 1991)
+aniversario.dia = 4
+
+console.log(aniversario)
