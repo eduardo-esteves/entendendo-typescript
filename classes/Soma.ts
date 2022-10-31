297e887604c479e9f73c9b07211b4e028762e6ea
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2022-10-31 10:53:10 -0300

    example of polymorphism with the use of inherit

--- classes/Soma.ts
+++ classes/Soma.ts
@@ -6,6 +6,16 @@ class Soma extends Calculo {
   }
 }
 
-const soma = new Soma()
+class Multiplicar extends Calculo {
+  executar (...numeros: number[]): void {
+    this.resultado = numeros.reduce((soma, val) => soma * val)
+  }
+}
+
+let soma: Calculo = new Soma()
+soma.executar(10, 20, 30)
+console.log(soma.getResultado())
+
+soma = new Multiplicar()
 soma.executar(10, 20, 30)
 console.log(soma.getResultado())
