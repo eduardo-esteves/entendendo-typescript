eb5c2039e124d70b4e214616b9f131c7c49c6b3d
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2022-11-07 19:45:55 -0300

    add function assign at an interface

--- interfaces/personPlus.ts
+++ interfaces/personPlus.ts
@@ -3,26 +3,31 @@ interface PersonP {
   idade: number
   profissao?: string
   [prop: string]: any
+  saudar: (pessoa: PersonP) => void
 }
 
 function saudarPersonP (pessoa: PersonP): void {
-  console.log(`Olá ${pessoa.nome} voce tem ${pessoa.idade} anos `)
+  pessoa.saudar(pessoa)
 }
 
 function setPersonP (pessoa: PersonP): void {
   pessoa.nome = 'Janaina'
   pessoa.idade = 30
-  console.log(`Olá ${pessoa.nome} voce tem ${pessoa.idade} anos `)
+  pessoa.saudar(pessoa)
 }
 
 const peopleP: PersonP = {
   nome: 'João',
-  idade: 27
+  idade: 27,
+  saudar (pessoa: PersonP): void {
+    console.log(`Olá ${pessoa.nome} voce tem ${pessoa.idade} anos `)
+  }
 }
 
 saudarPersonP(peopleP)
 setPersonP({
   nome: 'Eduardo',
   idade: 30,
-  xyz: 123
+  xyz: 123,
+  saudar: peopleP.saudar
 })
