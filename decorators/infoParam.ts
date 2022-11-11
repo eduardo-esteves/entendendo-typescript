be8d048590821196302e82bb62d1f730f61d6a20
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2022-11-11 17:06:31 -0300

    developed decorator to get info of parameters of class

---
+++ decorators/infoParam.ts
@@ -0,0 +1,6 @@
+export function infoParam (alvo: any, nomeMetodo: string, indiceParam: number): void {
+  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+  console.log(`Alvo: ${alvo}`)
+  console.log(`Método: ${nomeMetodo}`)
+  console.log(`Índice Param: ${indiceParam}`)
+}
