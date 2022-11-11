936a9a64846c83056ed121ffd165b0065550d034
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2022-11-11 16:28:48 -0300

    example of object freezer  behavior applied in function of an object class

---
+++ decorators/freeze-method.ts
@@ -0,0 +1,38 @@
+class ContaCorrente {
+  private saldo: number
+
+  constructor (saldo: number) {
+    this.saldo = saldo
+  }
+
+  @congelar
+  sacar (valor: number): boolean {
+    if (valor <= this.saldo) {
+      this.saldo -= valor
+      return true
+    }
+    return false
+  }
+
+  @congelar
+  getSaldo (): number {
+    return this.saldo
+  }
+}
+
+const cc = new ContaCorrente(10000)
+cc.sacar(5000)
+console.log(cc.getSaldo())
+
+// cc.getSaldo = function () {
+//   // eslint-disable-next-line @typescript-eslint/dot-notation
+//   return this['saldo'] + 7000
+// }
+
+console.log(cc.getSaldo())
+
+function congelar (alvo: any, nomeMetodo: string, descritor: PropertyDescriptor): void {
+  console.log(alvo)
+  console.log(nomeMetodo)
+  descritor.writable = false
+}
